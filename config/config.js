4375ce38e9fa551673ae545b8a748b9f8acb841f
arricodyanto <arricohandyanto@gmail.com>
2023-05-19 10:23:42 +0700

    Fix the config of the db connection & sequelize config

--- config/config.js
+++ config/config.js
@@ -1,14 +1,28 @@
 require('dotenv').config()
 
-const { Sequelize } = require('sequelize')
-const database = process.env.DB_NAME
-const username = process.env.DB_USERNAME
-const password = process.env.DB_PASSWORD
-
-const sequelize = new Sequelize({
-    database, username, password,
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-})
-
-module.exports = sequelize
+module.exports = {
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql'
+    },
+    test: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql'
+    },
+    production: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql'
+    }
+}
