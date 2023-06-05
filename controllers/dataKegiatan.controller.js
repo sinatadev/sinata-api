c143ecd3bceddb53436e96c9484b3af5bd01646b
arricodyanto <arricohandyanto@gmail.com>
2023-06-06 00:30:25 +0700

    Add tambah, edit, dan delete endpoint untuk tabel tb_kegiatan

--- controllers/dataKegiatan.controller.js
+++ controllers/dataKegiatan.controller.js
@@ -25,9 +25,65 @@ module.exports = {
     },
     addDataKegiatan: async(req, res) => {
         try {
-            
+            const payload = req.body
+
+            const kegiatan = await Kegiatans.create(payload)
+            await kegiatan.save()
+
+            res.status(201).json({
+                message: `Kegiatan '${payload.judul_kegiatan}' berhasil ditambahkan.`,
+                data: kegiatan
+            })
         } catch (error) {
+            res.status(500).json({
+                message: error.message || 'Internal Server Error'
+            })
+        }
+    },
+    editDataKegiatan: async(req, res) => {
+        const { id } = req.params
+        const payload = req.body
+        try {
+            const kegiatan = await Kegiatans.findByPk(id)
+            if(kegiatan){
+                Object.assign(kegiatan, payload)
+                await kegiatan.save()
+
+                res.status(200).json({
+                    message: `Kegiatan '${kegiatan.judul_kegiatan}' berhasil diperbarui`,
+                    data: kegiatan
+                })
+            } else {
+                res.status(404).json({
+                    message: 'Data kegiatan tidak ditemukan.'
+                })
+            }  
+        } catch (error) {
+            res.status(500).json({
+                message: error.message || 'Internal Server Error'
+            })
+        }
+    },
+    deleteDataKegiatan: async(req, res) => {
+        const { id } = req.params
+        try {
+            const kegiatan = await Kegiatans.findByPk(id)
             
+            if(kegiatan){
+                await kegiatan.destroy()
+                
+                res.status(200).json({
+                    message: `Kegiatan '${kegiatan.judul_kegiatan}' berhasil dihapus.`
+                })
+            } else {
+                res.status(404).json({
+                    message: 'Data kegiatan tidak ditemukan.'
+                })
+            }
+        } catch (error) {
+            res.status(500).json({
+                message: error.message || 'Internal Server Error'
+            })
         }
     }
 }
