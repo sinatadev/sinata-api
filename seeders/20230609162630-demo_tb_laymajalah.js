2c0d0293d6b6bfeeca0479f6057c2069216b7c39
arricodyanto <arricohandyanto@gmail.com>
2023-06-09 23:31:59 +0700

    Add seeders of all tables

--- seeders/20230609162630-demo_tb_laymajalah.js
+++ seeders/20230609162630-demo_tb_laymajalah.js
@@ -22,23 +22,11 @@ async function generateLayananMajalahData() {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    const majalahs = await generateLayananMajalahData()
+    await queryInterface.bulkInsert('tb_laymajalah', majalahs, {})
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('tb_laymajalah', null, {})
   }
 };
