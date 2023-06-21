bea9ae2107b96f62ac5a05e821c1dcd4a22946f2
arricodyanto <arricohandyanto@gmail.com>
2023-06-21 09:11:15 +0700

    Update the model, migration, and seeder of tb_account by add default image for img_profil

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,95 +1,99 @@
-const Accounts = require('../models/tb_account')
-const jwt = require('jsonwebtoken')
-const { hashPassword, compareSyncPassword } = require('../utils/password.util')
-const { jwtKey } = require('../config/config')
+const Accounts = require('../models/tb_account');
+const jwt = require('jsonwebtoken');
+const { hashPassword, compareSyncPassword } = require('../utils/password.util');
+const { jwtKey } = require('../config/config');
 
 module.exports = {
-    signup: async(req, res) => {
-        try {
-            const { username, name, email, password } = req.body
+  signup: async (req, res) => {
+    try {
+      const { username, name, email, password } = req.body;
 
-            const checkEmail = await Accounts.findOne({ where: { email: email }})
-            const checkUsername = await Accounts.findOne({ where: { username: username }})
+      const checkEmail = await Accounts.findOne({ where: { email: email } });
+      const checkUsername = await Accounts.findOne({
+        where: { username: username },
+      });
 
-            if(checkEmail && !checkUsername || checkEmail && checkUsername){
-                res.status(409).json({
-                    message: 'Email yang Anda masukkan sudah terdaftar.'
-                })
-            }
-            
-            if(checkUsername && !checkEmail){
-                res.status(409).json({
-                    message: 'Username yang Anda masukkan sudah terdaftar.'
-                })
-            }
+      if ((checkEmail && !checkUsername) || (checkEmail && checkUsername)) {
+        res.status(409).json({
+          message: 'Email yang Anda masukkan sudah terdaftar.',
+        });
+      }
 
-            if(!checkEmail && !checkUsername) {
-                const hashedPassword = await hashPassword(password)
-                
-                const newUser = await Accounts.create({
-                    username, 
-                    name, 
-                    email, 
-                    password:hashedPassword
-                })
-                await newUser.save()
-    
-                delete newUser.dataValues.password
-    
-                res.status(201).json({
-                    message: 'Akun berhasil di daftarkan',
-                    data: newUser
-                })
-            }
+      if (checkUsername && !checkEmail) {
+        res.status(409).json({
+          message: 'Username yang Anda masukkan sudah terdaftar.',
+        });
+      }
 
-        } catch (error) {
-            res.status(500).json({
-                message: 'Internal Server Error',
-                error: error.message
-            })
-        }
-    },
-    signin: async (req, res) => {
-        try {
-            const { email, password } = req.body
+      if (!checkEmail && !checkUsername) {
+        const hashedPassword = await hashPassword(password);
 
-            await Accounts.findOne({ where: { email: email }}).then((user) =>{
-                if(user) {
-                    const checkPassword = compareSyncPassword(password, user.password)
-                    if(checkPassword) {
-                        const token = jwt.sign({
-                            account: {
-                                id: user.id,
-                                username: user.username,
-                                name: user.name,
-                                email: user.email,
-                                unit: user.unit,
-                                role: user.role,
-                                img_profil: user.img_profil
-                            }
-                        }, jwtKey, { expiresIn: '24h' })
-                        
-                        res.status(200).json({
-                            message: 'Berhasil login',
-                            token: token
-                        })
-                    } else {
-                        res.status(403).json({
-                            message: 'Kata Sandi yang Anda masukkan salah.'
-                        })
-                    }
-                } else {
-                    res.status(403).json({
-                        message: 'Alamat Email yang Anda masukkan belum terdaftar.'
-                    })
-                }
-            })
+        const newUser = await Accounts.create({
+          username,
+          name,
+          email,
+          password: hashedPassword,
+        });
+        await newUser.save();
 
-        } catch (error) {
-            res.status(500).json({
-                message: 'Internal Server Error',
-                error: error.message
-            })
-        }
+        delete newUser.dataValues.password;
+
+        res.status(201).json({
+          message: 'Akun berhasil di daftarkan',
+          data: newUser,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: 'Internal Server Error',
+        error: error.message,
+      });
     }
-}
+  },
+  signin: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      await Accounts.findOne({ where: { email: email } }).then((user) => {
+        if (user) {
+          const checkPassword = compareSyncPassword(password, user.password);
+          if (checkPassword) {
+            const token = jwt.sign(
+              {
+                account: {
+                  id: user.id,
+                  username: user.username,
+                  name: user.name,
+                  email: user.email,
+                  unit: user.unit,
+                  role: user.role,
+                  img_profil: user.img_profil,
+                },
+              },
+              jwtKey,
+              { expiresIn: '24h' },
+            );
+
+            res.status(200).json({
+              message: 'Berhasil login',
+              data: token,
+            });
+          } else {
+            res.status(403).json({
+              message: 'Kata Sandi yang Anda masukkan salah.',
+            });
+          }
+        } else {
+          res.status(403).json({
+            message: 'Alamat Email yang Anda masukkan belum terdaftar.',
+          });
+        }
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Internal Server Error',
+        error: error.message,
+      });
+    }
+  },
+};
--- models/tb_account.js
+++ models/tb_account.js
@@ -12,48 +12,64 @@ class tb_account extends Model {
     // define association here
   }
 }
-tb_account.init({
-  id: {
-    allowNull: false,
-    primaryKey: true,
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
+tb_account.init(
+  {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    username: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    no_identitas: {
+      type: DataTypes.STRING,
+    },
+    unit: {
+      type: DataTypes.STRING,
+    },
+    token: {
+      type: DataTypes.STRING,
+    },
+    role: {
+      type: DataTypes.ENUM(
+        'User',
+        'Super Admin',
+        'Admin Role 1',
+        'Admin Role 2',
+        'Admin Role 3',
+        'Admin Role 4',
+        'Admin Role 5',
+        'Admin Role 6',
+        'Admin Role 7',
+        'Admin Role 8',
+        'Admin Role 9',
+      ),
+      defaultValue: 'User',
+    },
+    kontak: {
+      type: DataTypes.STRING,
+    },
+    img_profil: {
+      type: DataTypes.STRING,
+      defaultValue: 'default.jpeg',
+    },
   },
-  username: {
-    type: DataTypes.STRING
+  {
+    sequelize,
+    modelName: 'tb_account',
+    freezeTableName: true,
   },
-  email: {
-    type: DataTypes.STRING
-  },
-  name: {
-    type: DataTypes.STRING
-  },
-  password: {
-    type: DataTypes.STRING
-  },
-  no_identitas: {
-    type: DataTypes.STRING
-  },
-  unit: {
-    type: DataTypes.STRING
-  },
-  token: {
-    type: DataTypes.STRING
-  },
-  role: {
-    type: DataTypes.ENUM('User', 'Super Admin', 'Admin Role 1', 'Admin Role 2', 'Admin Role 3', 'Admin Role 4', 'Admin Role 5', 'Admin Role 6', 'Admin Role 7', 'Admin Role 8', 'Admin Role 9'),
-    defaultValue: 'User',
-  },
-  kontak: {
-    type: DataTypes.STRING
-  },
-  img_profil: {
-    type: DataTypes.STRING
-  },
-}, {
-  sequelize,
-  modelName: 'tb_account',
-  freezeTableName: true
-})
+);
 
-module.exports = tb_account
+module.exports = tb_account;
