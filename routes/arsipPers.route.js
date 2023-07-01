f5b32b65edfc1ff5e553e44b6a11a97b3c1472ef
arricodyanto <arricohandyanto@gmail.com>
2023-07-02 06:41:40 +0700

    Update Arsip Pers API

--- routes/arsipPers.route.js
+++ routes/arsipPers.route.js
@@ -1,12 +1,19 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
-const { isLoginSuperAdmin } = require('../middlewares/auth.middleware')
-const { viewArsipPers, addArsipPers, editArsipPers, deleteArsipPers, viewPengumuman } = require('../controllers/arsipPers.controller')
+const { isLoginSuperAdmin } = require('../middlewares/auth.middleware');
+const {
+  viewArsipPers,
+  addArsipPers,
+  editArsipPers,
+  deleteArsipPers,
+  viewOneArsipPers,
+} = require('../controllers/arsipPers.controller');
 
-router.get('/lihat', isLoginSuperAdmin, viewArsipPers)
-router.post('/tambah', isLoginSuperAdmin, addArsipPers)
-router.put('/:id/edit', isLoginSuperAdmin, editArsipPers)
-router.delete('/:id/delete', isLoginSuperAdmin, deleteArsipPers)
+router.get('/lihat', isLoginSuperAdmin, viewArsipPers);
+router.get('/:id/lihat', isLoginSuperAdmin, viewOneArsipPers);
+router.post('/tambah', isLoginSuperAdmin, addArsipPers);
+router.put('/:id/edit', isLoginSuperAdmin, editArsipPers);
+router.delete('/:id/delete', isLoginSuperAdmin, deleteArsipPers);
 
-module.exports = router
+module.exports = router;
