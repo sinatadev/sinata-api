25603429fff3c35e4e21f6f93ece07582c9e54c8
arricodyanto <arricohandyanto@gmail.com>
2023-07-02 06:42:44 +0700

    Update Layanan Peliputan's API

--- routes/peliputan.route.js
+++ routes/peliputan.route.js
@@ -1,19 +1,36 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
-const { isLoginSuperAdmin } = require('../middlewares/auth.middleware')
-const { viewPeliputan, addPeliputan, editPeliputan, deletePeliputan } = require('../controllers/peliputan.controller')
-const upload = require('../utils/upload.util')
+const { isLoginSuperAdmin } = require('../middlewares/auth.middleware');
+const {
+  viewPeliputan,
+  addPeliputan,
+  editPeliputan,
+  deletePeliputan,
+  viewOnePeliputan,
+} = require('../controllers/peliputan.controller');
+const upload = require('../utils/upload.util');
 
-router.get('/lihat', isLoginSuperAdmin, viewPeliputan)
-router.post('/tambah', isLoginSuperAdmin, upload.fields([
+router.get('/lihat', isLoginSuperAdmin, viewPeliputan);
+router.get('/:id/lihat', isLoginSuperAdmin, viewOnePeliputan);
+router.post(
+  '/tambah',
+  isLoginSuperAdmin,
+  upload.fields([
     { name: 'leaflet_kegiatan', maxCount: 1 },
-    { name: 'disposisi', maxCount: 1 }
-]), addPeliputan)
-router.put('/:id/edit', isLoginSuperAdmin, upload.fields([
+    { name: 'disposisi', maxCount: 1 },
+  ]),
+  addPeliputan,
+);
+router.put(
+  '/:id/edit',
+  isLoginSuperAdmin,
+  upload.fields([
     { name: 'leaflet_kegiatan', maxCount: 1 },
-    { name: 'disposisi', maxCount: 1 }
-]), editPeliputan)
-router.delete('/:id/delete', isLoginSuperAdmin, deletePeliputan)
+    { name: 'disposisi', maxCount: 1 },
+  ]),
+  editPeliputan,
+);
+router.delete('/:id/delete', isLoginSuperAdmin, deletePeliputan);
 
-module.exports = router
+module.exports = router;
