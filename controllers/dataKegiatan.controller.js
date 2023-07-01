479c921582c903768f135f80618e1927406fb8cd
arricodyanto <arricohandyanto@gmail.com>
2023-07-02 06:42:16 +0700

    Update Data Kegiatan's API

--- controllers/dataKegiatan.controller.js
+++ controllers/dataKegiatan.controller.js
@@ -1,101 +1,119 @@
-const Accounts = require('../models/tb_account')
-const Kegiatans = require('../models/tb_kegiatan')
+const Accounts = require('../models/tb_account');
+const Kegiatans = require('../models/tb_kegiatan');
 
 module.exports = {
-    viewDataKegiatan: async(req, res) => {
-        const page = req.query.page || 1
-        const limit = parseInt(req.query.limit) || 5
-        const offset = (page - 1) * limit
-        try {
-            const totalRow = await Kegiatans.count()
-            const totalPage = Math.ceil(totalRow / limit)
-            const kegiatan = await Kegiatans.findAll({
-                include: {
-                    model: Accounts,
-                    required: true
-                },
-                limit: limit,
-                offset: offset,
-                order: [
-                    ['createdAt', 'DESC']
-                ]
-            })
-            res.status(200).json({
-                message: `Berhasil menampilkan ${kegiatan.length} data kegiatan tersimpan.`,
-                page,
-                totalPage,
-                totalRow,
-                rowsPerPage: limit,
-                data: kegiatan
-            })
-        } catch (error) {
-            res.status(500).json({
-                message: error.message || 'Internal Server Error'
-            })
-
-        }
-    },
-    addDataKegiatan: async(req, res) => {
-        try {
-            const payload = req.body
-
-            const kegiatan = await Kegiatans.create(payload)
-            await kegiatan.save()
-
-            res.status(201).json({
-                message: `Kegiatan '${payload.judul_kegiatan}' berhasil ditambahkan.`,
-                data: kegiatan
-            })
-        } catch (error) {
-            res.status(500).json({
-                message: error.message || 'Internal Server Error'
-            })
-        }
-    },
-    editDataKegiatan: async(req, res) => {
-        const { id } = req.params
-        const payload = req.body
-        try {
-            const kegiatan = await Kegiatans.findByPk(id)
-            if(kegiatan){
-                Object.assign(kegiatan, payload)
-                await kegiatan.save()
-
-                res.status(200).json({
-                    message: `Kegiatan '${kegiatan.judul_kegiatan}' berhasil diperbarui`,
-                    data: kegiatan
-                })
-            } else {
-                res.status(404).json({
-                    message: 'Data kegiatan tidak ditemukan.'
-                })
-            }  
-        } catch (error) {
-            res.status(500).json({
-                message: error.message || 'Internal Server Error'
-            })
-        }
-    },
-    deleteDataKegiatan: async(req, res) => {
-        const { id } = req.params
-        try {
-            const kegiatan = await Kegiatans.findByPk(id)
-            
-            if(kegiatan){
-                await kegiatan.destroy()
-                
-                res.status(200).json({
-                    message: `Kegiatan '${kegiatan.judul_kegiatan}' berhasil dihapus.`
-                })
-            } else {
-                res.status(404).json({
-                    message: 'Data kegiatan tidak ditemukan.'
-                })
-            }
-        } catch (error) {
-            res.status(500).json({
-                message: error.message || 'Internal Server Error'
-            })
-        }
+  viewDataKegiatan: async (req, res) => {
+    const page = req.query.page || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const offset = (page - 1) * limit;
+    try {
+      const totalRow = await Kegiatans.count();
+      const totalPage = Math.ceil(totalRow / limit);
+      const kegiatan = await Kegiatans.findAll({
+        include: {
+          model: Accounts,
+          required: true,
+        },
+        limit,
+        offset,
+        order: [['createdAt', 'DESC']],
+      });
+      res.status(200).json({
+        message: `Berhasil menampilkan ${kegiatan.length} data kegiatan tersimpan.`,
+        page,
+        totalPage,
+        totalRow,
+        rowsPerPage: limit,
+        data: kegiatan,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message || 'Internal Server Error',
+      });
     }
-}
+  },
+  viewOneDataKegiatan: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const kegiatan = await Kegiatans.findAll({
+        include: {
+          model: Accounts,
+          required: true,
+        },
+        where: { id },
+        order: [['createdAt', 'DESC']],
+      });
+      res.status(200).json({
+        message: `Berhasil menampilkan ${kegiatan.length} data kegiatan tersimpan.`,
+        data: kegiatan,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message || 'Internal Server Error',
+      });
+    }
+  },
+  addDataKegiatan: async (req, res) => {
+    try {
+      const payload = req.body;
+
+      const kegiatan = await Kegiatans.create(payload);
+      await kegiatan.save();
+
+      res.status(201).json({
+        message: `Kegiatan '${payload.judul_kegiatan}' berhasil ditambahkan.`,
+        data: kegiatan,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message || 'Internal Server Error',
+      });
+    }
+  },
+  editDataKegiatan: async (req, res) => {
+    const { id } = req.params;
+    const payload = req.body;
+    try {
+      const kegiatan = await Kegiatans.findByPk(id);
+      if (kegiatan) {
+        Object.assign(kegiatan, payload);
+        await kegiatan.save();
+
+        res.status(200).json({
+          message: `Kegiatan '${kegiatan.judul_kegiatan}' berhasil diperbarui`,
+          data: kegiatan,
+        });
+      } else {
+        res.status(404).json({
+          message: 'Data kegiatan tidak ditemukan.',
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: error.message || 'Internal Server Error',
+      });
+    }
+  },
+  deleteDataKegiatan: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const kegiatan = await Kegiatans.findByPk(id);
+
+      if (kegiatan) {
+        await kegiatan.destroy();
+
+        res.status(200).json({
+          message: `Kegiatan '${kegiatan.judul_kegiatan}' berhasil dihapus.`,
+        });
+      } else {
+        res.status(404).json({
+          message: 'Data kegiatan tidak ditemukan.',
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: error.message || 'Internal Server Error',
+      });
+    }
+  },
+};
--- routes/dataKegiatan.route.js
+++ routes/dataKegiatan.route.js
@@ -1,15 +1,21 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
-const { isLogin } = require('../middlewares/auth.middleware')
-const { viewDataKegiatan, addDataKegiatan, editDataKegiatan, deleteDataKegiatan } = require('../controllers/dataKegiatan.controller')
+const { isLogin } = require('../middlewares/auth.middleware');
+const {
+  viewDataKegiatan,
+  addDataKegiatan,
+  editDataKegiatan,
+  deleteDataKegiatan,
+} = require('../controllers/dataKegiatan.controller');
 
 // admins
-router.get('/lihat', isLogin, viewDataKegiatan)
-router.post('/tambah', isLogin, addDataKegiatan)
-router.put('/:id/edit', isLogin, editDataKegiatan)
-router.delete('/:id/delete', isLogin, deleteDataKegiatan)
+router.get('/lihat', isLogin, viewDataKegiatan),
+  router.get('/:id/lihat', isLogin, viewDataKegiatan);
+router.post('/tambah', isLogin, addDataKegiatan);
+router.put('/:id/edit', isLogin, editDataKegiatan);
+router.delete('/:id/delete', isLogin, deleteDataKegiatan);
 
 // users
 
-module.exports = router
+module.exports = router;
