e147ed7e6ee2f2c2992a83df7a122dcb4a3ca815
arricodyanto <arricohandyanto@gmail.com>
2023-07-18 20:03:24 +0700

    Add an endpoint to change users'login profile

--- routes/accounts.route.js
+++ routes/accounts.route.js
@@ -2,16 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 const {
-  viewUsers,
-  addUsers,
-  editUser,
-  deleteUser,
-  changeAvatar,
-  changePassword,
+	viewUsers,
+	addUsers,
+	editUser,
+	deleteUser,
+	changeAvatar,
+	changePassword,
+	changeProfile,
 } = require('../controllers/accounts.controller');
 const {
-  isLoginSuperAdmin,
-  isLogin,
+	isLoginSuperAdmin,
+	isLogin,
 } = require('../middlewares/auth.middleware');
 const upload = require('../utils/upload.util');
 
@@ -20,11 +21,12 @@ router.post('/tambah', isLoginSuperAdmin, addUsers);
 router.put('/:id/edit', isLoginSuperAdmin, editUser);
 router.delete('/:id/delete', isLoginSuperAdmin, deleteUser);
 router.put(
-  '/:id/change-avatar',
-  isLoginSuperAdmin,
-  upload.single('img_profil'),
-  changeAvatar,
+	'/:id/change-avatar',
+	isLoginSuperAdmin,
+	upload.single('img_profil'),
+	changeAvatar,
 );
+router.put('/:id/change-profile', isLogin, changeProfile);
 router.put('/:id/change-password', isLogin, changePassword);
 
 module.exports = router;
