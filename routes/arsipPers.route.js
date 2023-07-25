acae3001bb6ff0ac35c7a6f2eb0af80bbb7b0297
arricodyanto <arricohandyanto@gmail.com>
2023-07-25 22:09:14 +0700

    Update the auth of arsip pers endpoint

--- routes/arsipPers.route.js
+++ routes/arsipPers.route.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const { isLoginAdmin, isLogin } = require('../middlewares/auth.middleware');
+const {
+	isLoginAdmin,
+	isLogin,
+	isAuthArsipPers,
+} = require('../middlewares/auth.middleware');
 const {
 	viewArsipPers,
 	addArsipPers,
@@ -13,9 +17,9 @@ const {
 
 router.get('/lihat', isLoginAdmin, viewArsipPers);
 router.get('/:id/lihat', isLoginAdmin, viewOneArsipPers);
-router.post('/tambah', isLoginAdmin, addArsipPers);
-router.put('/:id/edit', isLoginAdmin, editArsipPers);
-router.delete('/:id/delete', isLoginAdmin, deleteArsipPers);
+router.post('/tambah', isAuthArsipPers, addArsipPers);
+router.put('/:id/edit', isAuthArsipPers, editArsipPers);
+router.delete('/:id/delete', isAuthArsipPers, deleteArsipPers);
 router.get('/user/:id_account/lihat', isLogin, viewArsipPersUser);
 
 module.exports = router;
