5cb4179a7c22075c86b2a9fd4083b21b9bd8d089
arricodyanto <arricohandyanto@gmail.com>
2023-07-28 02:50:47 +0700

    Update auth in route view Data Kegiatan

--- routes/dataKegiatan.route.js
+++ routes/dataKegiatan.route.js
@@ -1,36 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-const { isLogin } = require('../middlewares/auth.middleware');
+const { isLogin, isLoginAdmin } = require('../middlewares/auth.middleware');
 const {
-  viewDataKegiatan,
-  addDataKegiatan,
-  editDataKegiatan,
-  deleteDataKegiatan,
-  viewDataKegiatanUser,
+	viewDataKegiatan,
+	addDataKegiatan,
+	editDataKegiatan,
+	deleteDataKegiatan,
+	viewDataKegiatanUser,
 } = require('../controllers/dataKegiatan.controller');
 const upload = require('../utils/upload.util');
 
 // admins
-router.get('/lihat', isLogin, viewDataKegiatan);
+router.get('/lihat', isLoginAdmin, viewDataKegiatan);
 router.get('/:id/lihat', isLogin, viewDataKegiatan);
 router.post(
-  '/tambah',
-  isLogin,
-  upload.fields([
-    { name: 'surat_permohonan', maxCount: 1 },
-    { name: 'sik', maxCount: 1 },
-  ]),
-  addDataKegiatan,
+	'/tambah',
+	isLogin,
+	upload.fields([
+		{ name: 'surat_permohonan', maxCount: 1 },
+		{ name: 'sik', maxCount: 1 },
+	]),
+	addDataKegiatan,
 );
 router.put(
-  '/:id/edit',
-  isLogin,
-  upload.fields([
-    { name: 'surat_permohonan', maxCount: 1 },
-    { name: 'sik', maxCount: 1 },
-  ]),
-  editDataKegiatan,
+	'/:id/edit',
+	isLogin,
+	upload.fields([
+		{ name: 'surat_permohonan', maxCount: 1 },
+		{ name: 'sik', maxCount: 1 },
+	]),
+	editDataKegiatan,
 );
 router.delete('/:id/delete', isLogin, deleteDataKegiatan);
 
