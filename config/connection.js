b4d5d405c2e2088039736005d0bdf3f3cc602b08
arricodyanto <arricohandyanto@gmail.com>
2024-01-11 00:05:36 +0700

    :card_file_box: migrate: db from mysql to postgresql

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const { Sequelize } = require('sequelize')
-const database = process.env.DB_NAME
-const username = process.env.DB_USERNAME
-const password = process.env.DB_PASSWORD
+const { Sequelize } = require('sequelize');
+const database = process.env.DB_NAME;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 
 const sequelize = new Sequelize(database, username, password, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT
-})
+	host: process.env.DB_HOST,
+	dialect: process.env.DB_DIALECT,
+	port: process.env.DB_PORT,
+});
 
-module.exports = sequelize
+module.exports = sequelize;
